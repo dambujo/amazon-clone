f433a6641ee432fc8cae3b0462b1542d2de0fd09
Antonio Dambujo <pdambuja@gmail.com>
2021-12-21 14:12:59 +0200

    add links to page

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,13 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import Head from 'next/head';
 import React from 'react';
+import NextLink from 'next/link';
 import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
@@ -12,7 +19,20 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Amazon-clone</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Amazon-clone</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
